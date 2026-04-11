c6c4b52d409a22320bcf7579ffc9126da05a4c86
startklar-code <Algedad.mohamed@gmail.com>
2026-04-11 18:55:40 +0200

    Refactor tests to include convertToSAR function

--- script.test.js
+++ script.test.js
@@ -1,18 +1,14 @@
-// استيراد دالة sum من ملف script.js
-const { sum } = require('./script');
+// في بداية ملف script.test.js
+const { sum, convertToSAR } = require('./script');
 
 test('فحص دالة الجمع - النجاح الحقيقي', () => {
-    // الآن الدالة أصبحت معرفة وسيعمل الاختبار
     expect(sum(2, 2)).toBe(4); 
 });
-const { convertToSAR } = require('./script');
 
 test('فحص دقة تحويل العملة', () => {
-    // نتوقع أن 100 دولار تساوي 375 ريال
     expect(convertToSAR(100)).toBe(375);
 });
 
 test('فحص التعامل مع الأرقام السالبة', () => {
-    // نتوقع أن الرقم السالب يعطي صفر
     expect(convertToSAR(-10)).toBe(0);
 });
